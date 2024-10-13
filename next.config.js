f5066c01c0afa388e788fcfc0c708a81f7cf59c3
unknown <lucassewards@outlook.com>
2024-10-13 03:32:09 -0400

    updates

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  images: {
-    domains: ['4cs.gia.edu'],
-  },
-}
+  // Your Next.js configuration options go here
+};
 
-module.exports = nextConfig
+export default nextConfig;
